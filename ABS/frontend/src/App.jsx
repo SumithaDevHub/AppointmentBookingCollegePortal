af0ab8224b025d66a7e66c0a53577f7bdd5022cd
swetha-pappusamy <swethapappusamy.profile@gmail.com>
2025-04-13 18:33:15 +0530

    added home page

--- ABS/frontend/src/App.jsx
+++ ABS/frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+
 
 const App = () => {
   return (
-    <div>
-       <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center p-6">
-       <div className="bg-white dark:bg-gray-800 shadow-2xl rounded-2xl max-w-xl w-full p-8">
-       <h1 className="text-3xl font-bold text-blue-600 dark:text-blue-400 mb-4"> We have successfully setup the env! lets start with login</h1>
-       </div>
-       </div>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
